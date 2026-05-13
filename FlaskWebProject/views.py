354c7fbd764341259a5b0756fe4de17d31b72478
simonrichli <simonrichli89@gmail.com>
2026-05-13 09:32:33 +0200

    Update views.py
    added error log invalid credentials

--- FlaskWebProject/views.py
+++ FlaskWebProject/views.py
@@ -67,6 +67,11 @@ def login():
         user = User.query.filter_by(username=form.username.data).first()
         if user is None or not user.check_password(form.password.data):
             flash('Invalid username or password')
+            app.logger.error(
+            "Microsoft login FAILED | error=%s | ip=%s",
+            request.args.get("error"),
+            request.remote_addr
+            )
             return redirect(url_for('login'))
         login_user(user, remember=form.remember_me.data)
         next_page = request.args.get('next')
